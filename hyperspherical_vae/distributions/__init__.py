671c46c0772a2c92a602252311864c48c816bc44
nicola-decao <nicola.decao@uva.nl>
2020-03-21 16:06:41 +0000

    parallel sampling and approximated ive

--- hyperspherical_vae/distributions/__init__.py
+++ hyperspherical_vae/distributions/__init__.py
@@ -1,2 +1,4 @@
 from hyperspherical_vae.distributions.von_mises_fisher import VonMisesFisher
-from hyperspherical_vae.distributions.hyperspherical_uniform import HypersphericalUniform
+from hyperspherical_vae.distributions.hyperspherical_uniform import (
+    HypersphericalUniform,
+)
--- hyperspherical_vae/distributions/hyperspherical_uniform.py
+++ hyperspherical_vae/distributions/hyperspherical_uniform.py
@@ -1,4 +1,3 @@
-
 import math
 import torch
 
@@ -22,13 +21,21 @@ class HypersphericalUniform(torch.distributions.Distribution):
         self._device = val if isinstance(val, torch.device) else torch.device(val)
 
     def __init__(self, dim, validate_args=None, device="cpu"):
-        super(HypersphericalUniform, self).__init__(torch.Size([dim]), validate_args=validate_args)
+        super(HypersphericalUniform, self).__init__(
+            torch.Size([dim]), validate_args=validate_args
+        )
         self._dim = dim
         self.device = device
 
     def sample(self, shape=torch.Size()):
-        output = torch.distributions.Normal(0, 1).sample(
-            (shape if isinstance(shape, torch.Size) else torch.Size([shape])) + torch.Size([self._dim + 1])).to(self.device)
+        output = (
+            torch.distributions.Normal(0, 1)
+            .sample(
+                (shape if isinstance(shape, torch.Size) else torch.Size([shape]))
+                + torch.Size([self._dim + 1])
+            )
+            .to(self.device)
+        )
 
         return output / output.norm(dim=-1, keepdim=True)
 
@@ -36,11 +43,13 @@ class HypersphericalUniform(torch.distributions.Distribution):
         return self.__log_surface_area()
 
     def log_prob(self, x):
-        return - torch.ones(x.shape[:-1], device=self.device) * self.__log_surface_area()
+        return -torch.ones(x.shape[:-1], device=self.device) * self.__log_surface_area()
 
     def __log_surface_area(self):
         if torch.__version__ >= "1.0.0":
             lgamma = torch.lgamma(torch.tensor([(self._dim + 1) / 2]).to(self.device))
         else:
-            lgamma = torch.lgamma(torch.Tensor([(self._dim + 1) / 2], device=self.device))
+            lgamma = torch.lgamma(
+                torch.Tensor([(self._dim + 1) / 2], device=self.device)
+            )
         return math.log(2) + ((self._dim + 1) / 2) * math.log(math.pi) - lgamma
